ad03163dfd4760dd908b473e3ebe33932b052353
Usman <usman_9@outlook.com>
2020-09-08 03:36:37 +0500

    Delete function with dislaying id implemented

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,10 +1,18 @@
 import React, { Component } from "react";
 import { getMovies } from "../services/fakeMovieService";
+
 class Movies extends Component {
   state = { movies: getMovies() };
+
+  handleDelete(id) {
+    console.log(id);
+  }
   render() {
     return (
       <React.Fragment>
+        <p>
+          There are currently {this.state.movies.length} movies in database.
+        </p>
         <table className="table">
           <thead>
             <tr>
@@ -16,13 +24,17 @@ class Movies extends Component {
           </thead>
           <tbody>
             {this.state.movies.map((movie) => (
-              <tr>
+              <tr key={movie._id}>
                 <td>{movie.title}</td>
                 <td>{movie.genre.name}</td>
                 <td>{movie.numberInStock}</td>
                 <td>{movie.dailyRentalRate}</td>
                 <td>
-                  <button type="button" class="btn btn-danger btn-sm">
+                  <button
+                    type="button"
+                    className="btn btn-danger btn-sm"
+                    onClick={() => this.handleDelete(movie._id)}
+                  >
                     Delete
                   </button>
                 </td>
