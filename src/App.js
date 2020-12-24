a985999abb32a1f5170c063f4a09f8f33ef32db3
UsmanAfzl <58268898+UsmanAfzl@users.noreply.github.com>
2020-12-24 20:21:07 +0500

    login implemented

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { Component } from "react";
 import { Route, Redirect, Switch } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import jwtDecode from "jwt-decode";
 import NavBar from "./components/navbar";
 import Movies from "./components/movies.jsx";
 import Customers from "./components/customers";
@@ -8,31 +10,41 @@ import Rentals from "./components/rentals";
 import NotFound from "./components/notfound";
 import MovieForm from "./components/movieForm";
 import Form from "./components/form";
-import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 import RegisterForm from "./components/registerForm";
 import LoginForm from "./components/loginForm";
-function App() {
-  return (
-    <React.Fragment>
-      <ToastContainer />
-      <NavBar />
-      <main className="container">
-        <Switch>
-          <Route path="/movies/:id" component={MovieForm} />
-          <Route path="/form" component={Form} />
-          <Route path="/register" component={RegisterForm} />
-          <Route path="/login" component={LoginForm} />
-          <Route path="/movies" component={Movies}></Route>
-          <Route path="/customers" component={Customers}></Route>
-          <Route path="/rentals" component={Rentals}></Route>
-          <Route path="/not-found" component={NotFound}></Route>
-          <Redirect from="/" exact to="/movies" />
-          <Redirect to="/not-found" />
-        </Switch>
-      </main>
-    </React.Fragment>
-  );
+
+class App extends Component {
+  state = {};
+  componentDidMount() {
+    try {
+      const jwt = localStorage.getItem("token");
+      const user = jwtDecode(jwt);
+      this.setState({ user });
+    } catch (ex) {}
+  }
+  render() {
+    return (
+      <React.Fragment>
+        <ToastContainer />
+        <NavBar user={this.state.user} />
+        <main className="container">
+          <Switch>
+            <Route path="/movies/:id" component={MovieForm} />
+            <Route path="/form" component={Form} />
+            <Route path="/register" component={RegisterForm} />
+            <Route path="/login" component={LoginForm} />
+            <Route path="/movies" component={Movies}></Route>
+            <Route path="/customers" component={Customers}></Route>
+            <Route path="/rentals" component={Rentals}></Route>
+            <Route path="/not-found" component={NotFound}></Route>
+            <Redirect from="/" exact to="/movies" />
+            <Redirect to="/not-found" />
+          </Switch>
+        </main>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
