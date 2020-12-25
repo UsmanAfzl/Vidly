bf93af74dc38c9cac881a1243f32b3d41731b636
UsmanAfzl <58268898+UsmanAfzl@users.noreply.github.com>
2020-12-25 16:23:40 +0500

    Code refactored

--- src/App.js
+++ src/App.js
@@ -1,27 +1,25 @@
 import React, { Component } from "react";
 import { Route, Redirect, Switch } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import jwtDecode from "jwt-decode";
 import NavBar from "./components/navbar";
 import Movies from "./components/movies.jsx";
 import Customers from "./components/customers";
 import Rentals from "./components/rentals";
 import NotFound from "./components/notfound";
 import MovieForm from "./components/movieForm";
-import Form from "./components/form";
-import "./App.css";
 import RegisterForm from "./components/registerForm";
 import LoginForm from "./components/loginForm";
+import Logout from "./components/logout";
+import Form from "./components/form";
+import "react-toastify/dist/ReactToastify.css";
+import "./App.css";
+import auth from "./services/authService";
 
 class App extends Component {
   state = {};
   componentDidMount() {
-    try {
-      const jwt = localStorage.getItem("token");
-      const user = jwtDecode(jwt);
-      this.setState({ user });
-    } catch (ex) {}
+    const user = auth.getCurrentUser();
+    this.setState({ user });
   }
   render() {
     return (
@@ -34,6 +32,7 @@ class App extends Component {
             <Route path="/form" component={Form} />
             <Route path="/register" component={RegisterForm} />
             <Route path="/login" component={LoginForm} />
+            <Route path="/logout" component={Logout} />
             <Route path="/movies" component={Movies}></Route>
             <Route path="/customers" component={Customers}></Route>
             <Route path="/rentals" component={Rentals}></Route>
--- src/services/authService.js
+++ src/services/authService.js
@@ -1,8 +1,34 @@
 import http from "./httpService";
 import { apiUrl } from "../config.json";
+import jwtDecode from "jwt-decode";
 
 const apiEndpoint = apiUrl + "/auth";
-
-export function Login(email, password) {
-  return http.post(apiEndpoint, { email, password });
+const tokenKey = "key";
+export async function login(email, password) {
+  const { data: jwt } = await http.post(apiEndpoint, { email, password });
+  localStorage.setItem(tokenKey, jwt);
 }
+
+export function loginWithJwt(jwt) {
+  localStorage.setItem(tokenKey, jwt);
+}
+
+export function logout() {
+  localStorage.removeItem(tokenKey);
+}
+
+export function getCurrentUser() {
+  try {
+    const jwt = localStorage.getItem(tokenKey);
+    return jwtDecode(jwt);
+  } catch (ex) {
+    return null;
+  }
+}
+
+export default {
+  login,
+  logout,
+  getCurrentUser,
+  loginWithJwt,
+};
